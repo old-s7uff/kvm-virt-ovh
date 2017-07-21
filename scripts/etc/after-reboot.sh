a476e37a20464edc0a0603f4a87da09365136eb7
ƬHE ЯAW  ☣ <jasht.sserie@gmail.com>
2017-07-21 17:04:22 +0000

    Update after-reboot.sh

--- scripts/etc/after-reboot.sh
+++ scripts/etc/after-reboot.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
+
+# move libvirt rules..
 mv /opt/libvirt/ /etc/
+
+#Start libvirt! Rules will apply (iptables, network)
+service libvirt-bin stop
+service libvirt-bin start
+
+#Flush libvirt firewall rules, and apply yours!
+sh /etc/scripts/iptable-rules
+
+
