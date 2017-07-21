d0d643b21f8b65607909ea2ae484df6602486252
ƬHE ЯAW  ☣ <jasht.sserie@gmail.com>
2017-07-21 19:01:02 +0000

    Update before-reboot.sh

--- scripts/etc/before-reboot.sh
+++ scripts/etc/before-reboot.sh
@@ -5,4 +5,5 @@ mv /etc/init.d/virtnetwork /opt/
 mv /etc/init/libvirt-bin.conf /opt/
 mv /etc/init.d/virtualizor /opt/
 mv /etc/init.d/zzvirtservice /opt/
+service virtnetwork stop
 service isc-dhcp-server stop
