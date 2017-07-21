f138a202d4192f6c8f78531e2f34c714930c883b
ƬHE ЯAW  ☣ <jasht.sserie@gmail.com>
2017-07-21 18:21:39 +0000

    Update after-reboot.sh

--- scripts/etc/after-reboot.sh
+++ scripts/etc/after-reboot.sh
@@ -2,10 +2,15 @@
 
 # move libvirt rules..
 mv /opt/libvirt/ /etc/
+mv /opt/virtnetwork /etc/init.d/
+mv /opt/libvirt-bin.conf /etc/init/
+mv /opt/virtualizor /etc/init.d/
+mv /opt/zzvirtservice /etc/init.d/
 
 #Start libvirt! Rules will apply (iptables, network)
 service libvirt-bin stop
 service libvirt-bin start
+service isc-dhcp-server start
 
 #Flush libvirt firewall rules, and apply yours!
 sh /etc/scripts/iptable-rules.sh
