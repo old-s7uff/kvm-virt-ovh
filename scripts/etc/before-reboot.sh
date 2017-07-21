50dfc2be96f3de2d405a86273f434290c218ed87
ƬHE ЯAW  ☣ <jasht.sserie@gmail.com>
2017-07-21 18:21:26 +0000

    Update before-reboot.sh

--- scripts/etc/before-reboot.sh
+++ scripts/etc/before-reboot.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 # So libvirt will not apply any kind of rule on server boot!
 mv /etc/libvirt/ /opt/
+mv /etc/init.d/virtnetwork /opt/
+mv /etc/init/libvirt-bin.conf /opt/
+mv /etc/init.d/virtualizor /opt/
+mv /etc/init.d/zzvirtservice /opt/
+service isc-dhcp-server stop
